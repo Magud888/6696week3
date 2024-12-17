f566ab8f5ed02e15dd6883a84fc1c4e7bb5ecf36
Magud <punrop5@gmail.com>
2024-12-17 10:44:44 +0700

    Ex1

---
+++ ex2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- เพิ่ม Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <style>
+        body{
+  font-family: "Sriracha", serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+    </style>
+    <!--  สิ้นสุดเพิ่ม font -->
+    <title>ตัวอย่าง1</title>
+</head>
+<body>
+    <h1> โปรแกรม ภาษา HTML </h1>
+    664485001 นาย กริชกร ปีคาน <br>
+    หมู่เรียน 66/96 <br>
+    
+</body>
+</html>
